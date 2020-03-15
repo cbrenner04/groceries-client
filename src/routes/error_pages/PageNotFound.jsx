b6323ad3f2063dd35de4d09495abfa813e94f6ae
cbrenner04 <cbrenner265@gmail.com>
2020-03-15 13:02:20 -0500

    Fix vulnerability

--- src/routes/error_pages/PageNotFound.jsx
+++ src/routes/error_pages/PageNotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
   return (
-    <div>
+    <>
       <h1>Page not found!</h1>
       <h2>Sorry but the page you are looking for was not found.</h2>
       <Link to="/">Return to the home page</Link>
-    </div>
+    </>
   );
 }
