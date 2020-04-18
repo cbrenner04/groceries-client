63f83852e473e9bdc3579500e7a6654a006632e8
cbrenner04 <cbrenner265@gmail.com>
2020-04-18 16:35:55 -0500

    Update PUBLIC_URL, add serviceWorker back and register

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 import AppRouter from './AppRouter';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
