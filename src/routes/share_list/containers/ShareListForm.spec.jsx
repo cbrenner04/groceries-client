356db67463c4fe5f14ee67fd18ef8a6f0c95884f
cbrenner04 <cbrenner265@gmail.com>
2020-06-28 17:14:06 -0500

    Update sharing

--- src/routes/share_list/containers/ShareListForm.spec.jsx
+++ src/routes/share_list/containers/ShareListForm.spec.jsx
@@ -244,97 +244,273 @@ describe('ShareListForm', () => {
     expect(toast).toHaveBeenCalledWith('failed to send request', { type: 'error' });
   });
 
-  it('toggles permissions in pending', async () => {
-    axios.patch = jest.fn().mockResolvedValue({});
+  describe('toggles permissions', () => {
+    it('toggles permissions in pending', async () => {
+      axios.patch = jest.fn().mockResolvedValue({});
 
-    const { getByTestId } = renderShareListForm(props);
+      const { getByTestId, getAllByTestId } = renderShareListForm(props);
 
-    expect(getByTestId('pending-user-2')).toHaveTextContent('read');
+      expect(getByTestId('pending-user-2')).toHaveTextContent('read');
 
-    fireEvent.click(getByTestId('pending-user-2').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      fireEvent.click(getAllByTestId('toggle-permissions')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
 
-    expect(getByTestId('pending-user-2')).toHaveTextContent('write');
+      expect(getByTestId('pending-user-2')).toHaveTextContent('write');
+    });
+
+    it('toggles permissions in accepted', async () => {
+      axios.patch = jest.fn().mockResolvedValue({});
+
+      const { getByTestId, getAllByTestId } = renderShareListForm(props);
+
+      expect(getByTestId('accepted-user-3')).toHaveTextContent('write');
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(getByTestId('accepted-user-3')).toHaveTextContent('read');
+    });
+
+    it('redirects to login on 401 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 401 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('You must sign in', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/users/sign_in');
+    });
+
+    it('redirects to lists on 403 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 403 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('You do not have permission to take that action', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/lists');
+    });
+
+    it('shows errors on 404 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 404 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('User not found', { type: 'error' });
+    });
+
+    it('shows errors on non 401, 403, 404 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 500, data: { foo: 'bar', foobar: 'foobaz' } } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('foo bar and foobar foobaz', { type: 'error' });
+    });
+
+    it('displays error on failed request from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ request: 'failed to send request' });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('Something went wrong', { type: 'error' });
+    });
+
+    it('displays error on unknown error from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ message: 'failed to send request' });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('toggle-permissions')[1]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('failed to send request', { type: 'error' });
+    });
   });
 
-  it('toggles permissions in accepted', async () => {
-    axios.patch = jest.fn().mockResolvedValue({});
+  describe('refresh share', () => {
+    it('refreshes share', async () => {
+      axios.patch = jest.fn().mockResolvedValue({ data: { id: 1, permissions: 'write' } });
 
-    const { getByTestId } = renderShareListForm(props);
+      const { getByTestId, getAllByTestId, queryByTestId } = renderShareListForm(props);
 
-    expect(getByTestId('accepted-user-3')).toHaveTextContent('write');
+      expect(getByTestId('refused-user-4')).toHaveTextContent('foobar@example.com');
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
 
-    expect(getByTestId('accepted-user-3')).toHaveTextContent('read');
+      expect(queryByTestId('refused-user-4')).toBeNull();
+    });
+
+    it('redirects to login on 401 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 401 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('You must sign in', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/users/sign_in');
+    });
+
+    it('redirects to lists on 403 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 403 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('You do not have permission to take that action', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/lists');
+    });
+
+    it('shows errors on 404 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 404 } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('User not found', { type: 'error' });
+    });
+
+    it('shows errors on non 401, 403, 404 from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ response: { status: 500, data: { foo: 'bar', foobar: 'foobaz' } } });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('foo bar and foobar foobaz', { type: 'error' });
+    });
+
+    it('displays error on failed request from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ request: 'failed to send request' });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('Something went wrong', { type: 'error' });
+    });
+
+    it('displays error on unknown error from toggling permissions', async () => {
+      axios.patch = jest.fn().mockRejectedValue({ message: 'failed to send request' });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('refresh-share')[0]);
+      await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('failed to send request', { type: 'error' });
+    });
   });
 
-  it('redirects to login on 401 from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ response: { status: 401 } });
+  describe('remove share', () => {
+    it('removes share', async () => {
+      axios.delete = jest.fn().mockResolvedValue({});
+      axios.get = jest.fn().mockResolvedValue({
+        data: {
+          accepted: props.accepted,
+          invitable_users: props.invitableUsers,
+          pending: [],
+          refused: props.refused,
+        },
+      });
 
-    const { getByTestId } = renderShareListForm(props);
+      const { getByTestId, getAllByTestId, queryByTestId } = renderShareListForm(props);
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      expect(getByTestId('pending-user-2')).toHaveTextContent('bar@example.com');
 
-    expect(toast).toHaveBeenCalledWith('You must sign in', { type: 'error' });
-    expect(props.history.push).toHaveBeenCalledWith('/users/sign_in');
-  });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
 
-  it('redirects to lists on 403 from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ response: { status: 403 } });
+      expect(queryByTestId('pending-user-2')).toBeNull();
+    });
 
-    const { getByTestId } = renderShareListForm(props);
+    it('redirects to login on 401 from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ response: { status: 401 } });
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      const { getAllByTestId } = renderShareListForm(props);
 
-    expect(toast).toHaveBeenCalledWith('You do not have permission to take that action', { type: 'error' });
-    expect(props.history.push).toHaveBeenCalledWith('/lists');
-  });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
 
-  it('shows errors on 404 from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ response: { status: 404 } });
+      expect(toast).toHaveBeenCalledWith('You must sign in', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/users/sign_in');
+    });
 
-    const { getByTestId } = renderShareListForm(props);
+    it('redirects to lists on 403 from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ response: { status: 403 } });
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      const { getAllByTestId } = renderShareListForm(props);
 
-    expect(toast).toHaveBeenCalledWith('User not found', { type: 'error' });
-  });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
 
-  it('shows errors on non 401, 403, 404 from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ response: { status: 500, data: { foo: 'bar', foobar: 'foobaz' } } });
+      expect(toast).toHaveBeenCalledWith('You do not have permission to take that action', { type: 'error' });
+      expect(props.history.push).toHaveBeenCalledWith('/lists');
+    });
 
-    const { getByTestId } = renderShareListForm(props);
+    it('shows errors on 404 from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ response: { status: 404 } });
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      const { getAllByTestId } = renderShareListForm(props);
 
-    expect(toast).toHaveBeenCalledWith('foo bar and foobar foobaz', { type: 'error' });
-  });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
 
-  it('displays error on failed request from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ request: 'failed to send request' });
+      expect(toast).toHaveBeenCalledWith('User not found', { type: 'error' });
+    });
 
-    const { getByTestId } = renderShareListForm(props);
+    it('shows errors on non 401, 403, 404 from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ response: { status: 500, data: { foo: 'bar', foobar: 'foobaz' } } });
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      const { getAllByTestId } = renderShareListForm(props);
 
-    expect(toast).toHaveBeenCalledWith('Something went wrong', { type: 'error' });
-  });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
 
-  it('displays error on unknown error from toggling permissions', async () => {
-    axios.patch = jest.fn().mockRejectedValue({ message: 'failed to send request' });
+      expect(toast).toHaveBeenCalledWith('foo bar and foobar foobaz', { type: 'error' });
+    });
 
-    const { getByTestId } = renderShareListForm(props);
+    it('displays error on failed request from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ request: 'failed to send request' });
 
-    fireEvent.click(getByTestId('accepted-user-3').firstChild);
-    await waitFor(() => expect(axios.patch).toHaveBeenCalledTimes(1));
+      const { getAllByTestId } = renderShareListForm(props);
 
-    expect(toast).toHaveBeenCalledWith('failed to send request', { type: 'error' });
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('Something went wrong', { type: 'error' });
+    });
+
+    it('displays error on unknown error from toggling permissions', async () => {
+      axios.delete = jest.fn().mockRejectedValue({ message: 'failed to send request' });
+
+      const { getAllByTestId } = renderShareListForm(props);
+
+      fireEvent.click(getAllByTestId('remove-share')[0]);
+      await waitFor(() => expect(axios.delete).toHaveBeenCalledTimes(1));
+
+      expect(toast).toHaveBeenCalledWith('failed to send request', { type: 'error' });
+    });
   });
 });
