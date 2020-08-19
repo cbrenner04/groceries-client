94bfc48d22f4d4d4c3dc2e3cbae8ae6224d99106
Christopher Brenner <cbrenner265@gmail.com>
2020-08-18 22:26:49 -0500

    Updates for csp (#25)
    * Updates for csp
    
    * Wow

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.scss';
 import AppRouter from './AppRouter';
-import * as serviceWorker from './serviceWorker';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
@@ -15,8 +14,3 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
