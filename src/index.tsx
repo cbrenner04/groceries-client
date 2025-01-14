25f92d7a5322a5d17c8efa59a61563074430cdbc
cbrenner04 <cbrenner265@gmail.com>
2025-01-13 20:42:27 -0600

    Revert "fix(deps): update dependency react-toastify to v11 (#534)"
    This reverts commit 607a5d195986a363deaa0dd14c543d51d9423255.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './index.scss';
 import AppRouter from './AppRouter';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const root: Root = createRoot(document.getElementById('root') as Element);
 root.render(
